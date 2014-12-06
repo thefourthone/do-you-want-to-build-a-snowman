5a86020007fb1b7003c60681d272ccf3b5c25e5c
Benjamin Landers <benjaminrlanders@gmail.com>
2014-12-06 02:48:10 -0800

    fixed silly error

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var main = document.getElementById('main');
 main.onkeydown = function(e){
   console.log(e.keyCode);
   if(e.keyCode < 37 || e.keyCode > 40){
-    type(String.fromCharCode(e.keyCode));
+    type(String.fromCharCode(e.keyCode))
     e.preventDefault();                     //prevent the defualt typing action
   }
 };
@@ -14,7 +14,7 @@ function type(char){
   var str = sel.anchorNode.data;          //get internal text
   var off = sel.anchorOffset;             //get current index
   if(str.substring(off)){                 //make sure not at the end
-    sel.anchorNode.data = str.substring(0,off) +String.fromCharCode(e.keyCode)+str.substring(off+1); //splice one character in where the cursor is
+    sel.anchorNode.data = str.substring(0,off) + char +str.substring(off+1); //splice one character in where the cursor is
     var range = document.createRange();   //create a new selection
     range.setStart(sel.anchorNode, off+1);
     range.setEnd(sel.anchorNode, off+1);
